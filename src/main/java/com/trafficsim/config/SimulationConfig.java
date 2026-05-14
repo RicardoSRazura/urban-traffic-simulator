a45f755c2471067cf2ff999b5ff54d07bbac0d9e
Ricardo Razura <srazura@getinsoft.com>
2026-05-14 11:58:34 -0600

    feat: implement config module

---
+++ src/main/java/com/trafficsim/config/SimulationConfig.java
@@ -0,0 +1,56 @@
+package com.trafficsim.config;
+
+public class SimulationConfig {
+
+    // Tiempos del semaforo en milisegundos
+    private int greenMs = 5000;
+    private int yellowMs = 2000;
+    private int redMs = 6000;
+
+    // Cantidad de vehiculos en la simulacion
+    private int vehicleCount = 30;
+
+    // Constructor con valores por defecto
+    public SimulationConfig() {}
+
+    // Constructor para cargar desde archivo o menu
+
+    public SimulationConfig(int greenMs, int yellowMs, int redMs, int vehicleCount) {
+        this.greenMs = greenMs;
+        this.yellowMs = yellowMs;
+        this.redMs = redMs;
+        this.vehicleCount = vehicleCount;
+    }
+
+    public int getGreenMs() {
+        return greenMs;
+    }
+
+    public void setGreenMs(int greenMs) {
+        this.greenMs = greenMs;
+    }
+
+    public int getYellowMs() {
+        return yellowMs;
+    }
+
+    public void setYellowMs(int yellowMs) {
+        this.yellowMs = yellowMs;
+    }
+
+    public int getRedMs() {
+        return redMs;
+    }
+
+    public void setRedMs(int redMs) {
+        this.redMs = redMs;
+    }
+
+    public int getVehicleCount() {
+        return vehicleCount;
+    }
+
+    public void setVehicleCount(int vehicleCount) {
+        this.vehicleCount = vehicleCount;
+    }
+}
